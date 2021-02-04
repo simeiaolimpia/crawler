eb52973dfe9d45e300e9728e477d8b04f9f2553b
Silvio <silvioej@gmail.com>
2021-02-04 14:49:48 -0300

    conecção com o mysql

---
+++ controller/bd.php
@@ -0,0 +1,8 @@
+<?php 
+
+$dsn = 'mysql:dbname=mydb;host=127.0.0.1'; // Data Source Name
+$db_user = '';
+$db_pass = '';
+$pdo = new PDO($dsn, $db_user, $db_pass); // representa a conexão com o SGBD
+
+ ?>
