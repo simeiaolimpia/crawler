ca25413232be6bb2f45809cd97657f5f12b7d798
Silvio <silvioej@gmail.com>
2021-02-04 14:48:26 -0300

    verificação de usuario cadastrado para login

---
+++ controller/login.php
@@ -0,0 +1,20 @@
+<?php require_once 'bd.php';
+
+$email = $_POST['email'];
+$password = md5(stripslashes($_POST['password']));
+
+
+$stmt = $pdo->prepare('SELECT * FROM cwl_usuario where usu_email = ? AND usu_senha = ? ');
+$stmt->execute([$email,$password]);
+$result = $stmt->rowCount();
+
+if ($result > 0) {
+
+header('Location:/view/home.php');
+exit();
+}else{
+	header('Location:/');
+}
+
+
+ ?>
