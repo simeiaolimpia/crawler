bc8b86cd98ef9059b133646662c1f90e4a3cd89c
K0rgana <anagrom1999@gmail.com>
2021-02-15 21:58:33 -0300

    adicionando template no feed

--- view/feed.php
+++ view/feed.php
@@ -1,1 +1,17 @@
-<h1>feed</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Feed</title>
+	<?php include 'temp/header.php'; ?>
+</head>
+<body>
+	<main>
+    <h1>feed</h1>
+    <a href="anu_detail.php"> detalhe do anúncio</a>
+  </main>
+
+<?php include 'temp/footer.php'; ?>
+</body>
+</html>
